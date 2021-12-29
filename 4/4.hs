eedaa0f6b08a21d4d51226e18c38de12c5cf8311
DarK-rtfm <44683230+DarK-rtfm@users.noreply.github.com>
2021-12-29 10:42:35 +0100

    4

--- 4/4.hs
+++ 4/4.hs
@@ -1,41 +1,55 @@
 import Data.List
+import Data.Maybe
 import Control.Monad
 import Data.List.Split
+import Distribution.Simple.Utils
+import Data.Bifunctor
 type Card = [[Int]]
-type File = [String]
+type Calls = [Int]
+type File = String
+type Win = (Card, Calls)
+type MaybeWin = (Card, Maybe Calls)
 
-steps' :: File -> [Int]
-steps'  = map read . splitOn "," . head
+-- PARSING
 
-steps :: File -> [[Int]]
-steps = tail . inits . steps'
+steps :: File -> [Calls]
+steps = tail . inits . steps' 
+    where
+        steps' :: File -> Calls
+        steps'  = map read . splitOn "," . head . lines
 
 cards :: File -> [Card]
-cards = (map . map) (map read . words) . splitOn [[]] . tail . tail
+cards = (map . map) (map read . words) . splitOn [[]] . tail . tail . lines
 
-winCheck :: [Int] -> Card -> Bool
-winCheck nums card = any (all (`elem` nums)) rows
+-- CHECKING IF CARD WINS
+winCheck :: Card -> Calls -> Bool
+winCheck card nums = any (all (`elem` nums)) rows
                   || any (all (`elem` nums)) cols
     where
         rows = card
         cols = transpose card
-        diagonal1 = map (\x -> card!!x!!x) [0.. length card]
-        diagonal2 = map (\x -> reverse card !!x !!x) [0.. length card]
 
-winsCheck :: [Int] -> [Card] -> Bool
-winsCheck seq= any (winCheck seq)
+-- CHECKING THE REQUIRED SEQUENCE OF CALLS FOR A CARD TO WIN, RETURN (Card, Nothing) IF IT NEVER WINS
+winsIn :: Card -> [Calls] -> MaybeWin
+winsIn card sx = (card , safeHead $ dropWhile (not . winCheck card) sx)
 
+-- FILTERING OUT ALL THE NON WINNER CARDS, SAYING WINNERS WITH THEIR SEQUENCE
+winners :: [Card] -> [Calls] -> [Win]
+winners cards sx = onlyWins $ map (`winsIn` sx) cards
+    where onlyWins = map (second fromJust). filter (isJust . snd)
 
-firstWinner :: [Card] -> [[Int]] -> (Card, [Int])
-firstWinner cards seqs = (head $ filter (winCheck winseq) cards, winseq)
-    where
-        winseq = head $ dropWhile (\x -> not $ winsCheck x cards) seqs
+-- SORT BASED ON THE LENGTH OF SEQUENCE NEEDED TO WIN
+sortWinners :: [Win] -> [Win]
+sortWinners = sortOn (length . snd)
 
-pointOfCard :: Card -> [Int] -> Int
+-- CALCULATE POINTS
+pointOfCard :: Card -> Calls -> Int
 pointOfCard card seq = (*) (last seq) . sum . filter (not . (`elem` seq)) $ concat card
 
 main :: IO()
 main = do
-    file <- readFile "input.txt"
+    wins <- sortWinners . liftM2 winners cards steps <$> readFile "input.txt"
     putStrLn "part 1"
-    print . uncurry pointOfCard . liftM2 firstWinner cards steps . lines $ file
+    print . uncurry pointOfCard . head $ wins
+    putStrLn "part 2"
+    print . uncurry pointOfCard . last $ wins
